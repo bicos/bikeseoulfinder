e4d82f0948be5d94d16410d125435ea6e7616c9c
ravy.park <ravy.park@kakaocorp.com>
2018-05-14 14:31:37 +0900

    Initial commit

---
+++ app/src/main/java/com/kakao/bikeseoulfinder/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.kakao.bikeseoulfinder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.kakao.bikeseoulfinder.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/kakao/bikeseoulfinder/ui/main/MainFragment.kt
@@ -0,0 +1,121 @@
+package com.kakao.bikeseoulfinder.ui.main
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.places.Places
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.kakao.bikeseoulfinder.R
+import pub.devrel.easypermissions.EasyPermissions
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.CameraUpdateFactory
+
+
+private const val REQ_GET_CURRENT_LOCATION = 1000
+
+private const val DEFAULT_ZOOM = 15f
+
+class MainFragment : Fragment(), OnMapReadyCallback, EasyPermissions.PermissionCallbacks, GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    private var map: GoogleMap? = null
+
+    private var googleApiClient: GoogleApiClient? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        (childFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment)?.getMapAsync(this)
+
+        activity?.let {
+            googleApiClient = GoogleApiClient.Builder(it)
+                    .enableAutoManage(it /* FragmentActivity */,
+                            this /* OnConnectionFailedListener */)
+                    .addConnectionCallbacks(this)
+                    .addApi(LocationServices.API)
+                    .addApi(Places.GEO_DATA_API)
+                    .addApi(Places.PLACE_DETECTION_API)
+                    .build().apply {
+                        connect()
+                    }
+        }
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+        Toast.makeText(context, p0.errorMessage, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onConnected(p0: Bundle?) {
+
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+
+    }
+
+    override fun onMapReady(map: GoogleMap?) {
+        this.map = map
+
+        val perm = Manifest.permission.ACCESS_FINE_LOCATION
+
+        context?.let {
+            if (EasyPermissions.hasPermissions(it, perm)) {
+                setMyLocationUi(true)
+                moveCurrentLocation()
+            } else {
+                EasyPermissions.requestPermissions(this, "현재 위치를 가져오기 위해 위치 접근 권한이 필요합니다.", REQ_GET_CURRENT_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
+        setMyLocationUi(true)
+        moveCurrentLocation()
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        setMyLocationUi(false)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun setMyLocationUi(enableMyLocation: Boolean) {
+        map?.isMyLocationEnabled = enableMyLocation
+        map?.uiSettings?.isMyLocationButtonEnabled = enableMyLocation
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun moveCurrentLocation() {
+        context?.let {
+            LocationServices.getFusedLocationProviderClient(it).lastLocation.addOnSuccessListener {
+                map?.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(it.latitude, it.longitude), DEFAULT_ZOOM))
+            }
+        }
+    }
+
+}
