ac25828e2a65d1fca479f3daeb67592361b6cb5a
ravy.park <ravy.park@kakaocorp.com>
2019-02-13 13:18:57 +0900

    1. 중간 커밋

--- app/src/main/java/com/kakao/bikeseoulfinder/ui/main/MainActivity.kt
+++ app/src/main/java/com/kakao/bikeseoulfinder/ui/main/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.kakao.bikeseoulfinder.ui.main
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.ArrayAdapter
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import com.kakao.bikeseoulfinder.R
@@ -12,6 +15,8 @@ import kotlinx.android.synthetic.main.main_activity.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +28,9 @@ class MainActivity : AppCompatActivity() {
                     .commitNow()
         }
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+
         val settingArray = resources.getStringArray(R.array.setting_array)
         left_drawer.adapter = ArrayAdapter<String>(this, R.layout.drawer_item, settingArray)
         left_drawer.setOnItemClickListener { _, _, position, _ ->
@@ -31,7 +39,28 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        drawerToggle = ActionBarDrawerToggle(this, drawer_layout, R.string.drawer_open, R.string.drawer_close)
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawer_layout.addDrawerListener(drawerToggle)
+
         viewModel = ViewModelProviders.of(this, MainViewModelFactory(this))
                 .get(MainViewModel::class.java)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
